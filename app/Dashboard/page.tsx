f56dd2973d0c8c396c529fbc59be17ebb32518df
Jayant Aggarwal <jadps2004@gmail.com>
2024-08-29 23:09:47 +0530

    dashboard ui updates

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 import React from "react";
 import Link from "next/link";
+import { Search, TrendingUp } from "lucide-react";
 import {
   BarChart,
   Bar,
-  PieChart,
-  Pie,
-  Cell,
   ResponsiveContainer,
+  Pie,
+  PieChart,
+  Cell,
 } from "recharts";
+import { Input } from "@/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 const Dashboard = () => {
   const barData = [
@@ -26,10 +36,41 @@ const Dashboard = () => {
   ];
 
   const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff7300", "#0088FE"];
+  const CustomLegend = () => (
+    <ul className="flex flex-col justify-center ml-6">
+      {pieData.map((entry, index) => (
+        <li key={`legend-${index}`} className="flex items-center mb-2">
+          <span
+            className="inline-block w-3 h-3 rounded-full mr-2"
+            style={{ backgroundColor: COLORS[index % COLORS.length] }}
+          ></span>
+          <span className="text-sm font-medium text-black">{entry.name}</span>
+          <span className="text-sm font-medium text-black ml-2">
+            {entry.value.toFixed(2)}
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
 
   return (
-    <div className="p-16 w-screen bg-white">
-      <h1 className="text-2xl font-bold mb-6 text-black">Dashboard</h1>
+    <div className="p-8 w-screen bg-white ">
+      <h1 className="text-2xl font-bold mb-6 text-black flex justify-center align-middle">
+        Dashboard
+      </h1>
+      <div className="flex justify-center align-middle text-black my-5 border border-gray-200 p-5">
+        <div className="relative w-[40%]">
+          <Search
+            className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
+            size={20}
+          />
+          <Input
+            type="search"
+            placeholder="Search"
+            className="pl-10 rounded-lg shadow-md py-6"
+          />
+        </div>
+      </div>
       <div className="grid grid-cols-3 gap-4 mb-8">
         <MetricCard title="Previous Month Order" value="3,458" link="/" />
         <MetricCard
@@ -54,31 +95,37 @@ const Dashboard = () => {
           </ResponsiveContainer>
         </div>
         <div className="shadow-md p-4 rounded-md">
-          <h2 className="text-lg text-black font-semibold mb-4">
-            % Reimbursements by Dispute Type - this year
-          </h2>
-          <ResponsiveContainer width="100%" height={300}>
-            <PieChart>
-              <Pie
-                data={pieData}
-                cx="50%"
-                cy="50%"
-                outerRadius={100}
-                fill="#8884d8"
-                dataKey="value"
-                label={({ name, percent }) =>
-                  `${name} ${(percent * 100).toFixed(0)}%`
-                }
-              >
-                {pieData.map((entry, index) => (
-                  <Cell
-                    key={`cell-${index}`}
-                    fill={COLORS[index % COLORS.length]}
-                  />
-                ))}
-              </Pie>
-            </PieChart>
-          </ResponsiveContainer>
+          <Card className="flex flex-col">
+            <CardHeader className="pb-0">
+              <CardTitle className="text-lg font-semibold text-black mb-4">
+                % Reimbursements by Dispute Type - this year
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="flex">
+              <ResponsiveContainer width="50%" height={300}>
+                <PieChart>
+                  <Pie
+                    data={pieData}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    innerRadius={50}
+                    paddingAngle={5}
+                    dataKey="value"
+                    cornerRadius={5}
+                  >
+                    {pieData.map((entry, index) => (
+                      <Cell
+                        key={`cell-${index}`}
+                        fill={COLORS[index % COLORS.length]}
+                      />
+                    ))}
+                  </Pie>
+                </PieChart>
+              </ResponsiveContainer>
+              <CustomLegend />
+            </CardContent>
+          </Card>
         </div>
       </div>
     </div>
